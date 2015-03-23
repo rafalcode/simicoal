e145d94ca50edc178b44fbd49c2998dbc1997705
Ramon Fallon <ramonfallon@gmail.com>
2015-03-23 13:34:22 +0100

    loops over until there is a single MRCA for all

--- simitwo.c
+++ simitwo.c
@@ -2,8 +2,8 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define PSZ 10 /* size of pop */
-#define GBUF 4
+#define IPSZ 22 /* initial size of pop */
+#define GBUF 2
 
 typedef struct
 {
@@ -15,53 +15,73 @@ typedef struct
 
 int main(int argc, char *argv[])
 {
-    int i, j;
-    int *ra=malloc(PSZ*sizeof(int));
+    int i, j, *ra, *pa;
+    acixs *pai;
+    int npcps=10; /* number of parent child pairs */
+    int pgbuf=GBUF;
+    int *pca=malloc(pgbuf*sizeof(int));
+    int pcpc=0; /* parent child pair counter */
+    pca[pcpc]=IPSZ;
 
-    int *pa=calloc(PSZ, sizeof(int)); /* parent array .. the number of siblings */
+    int pszc, gbuf;
 
-    for(j=0; j<PSZ; j++) {
-        ra[j]=(int)(PSZ*(((float)rand())/RAND_MAX));
-        pa[ra[j]]++;
-    }
+    while(pca[pcpc]>1) {
 
-    int pszc=0; /* parent array size counter */
-    int gbuf=GBUF;
-    acixs *pai=malloc(gbuf*sizeof(acixs));
+        ra=malloc(pca[pcpc]*sizeof(int));
+        pa=calloc(pca[pcpc], sizeof(int)); /* parent array .. the number of siblings */
 
-    for(j=0; j<PSZ; j++)
-        if(pa[j]) {
-            if(pszc ==gbuf-1) {
-                gbuf += GBUF;
-                pai=realloc(pai, gbuf*sizeof(acixs));
-            }
-            pai[pszc].il=j;
-            pai[pszc].casz=pa[j];
-            pai[pszc].ca=malloc(pai[pszc].casz*sizeof(int));
-            pszc++;
+        for(j=0; j<pca[pcpc]; j++) {
+            ra[j]=(int)(pca[pcpc]*(((float)rand())/RAND_MAX));
+            pa[ra[j]]++;
         }
 
-    free(pa);
-    int idx;
-    for(i=0;i<pszc;++i) {
-        idx=0;
-        for(j=0; j<PSZ; j++)
-           if(pai[i].il==ra[j])
-               pai[i].ca[idx++]=j;
+        pszc=0; /* parent array size counter */
+        gbuf=GBUF;
+        pai=malloc(gbuf*sizeof(acixs));
+
+        for(j=0; j<pca[pcpc]; j++)
+            if(pa[j]) {
+                if(pszc ==gbuf-1) {
+                    gbuf += GBUF;
+                    pai=realloc(pai, gbuf*sizeof(acixs));
+                }
+                pai[pszc].il=j;
+                pai[pszc].casz=pa[j];
+                pai[pszc].ca=malloc(pai[pszc].casz*sizeof(int));
+                pszc++;
+            }
+
+        free(pa);
+        int idx;
+        for(i=0;i<pszc;++i) {
+            idx=0;
+            for(j=0; j<pca[pcpc]; j++)
+                if(pai[i].il==ra[j])
+                    pai[i].ca[idx++]=j;
+        }
+
+        free(ra);
+
+        for(i=0;i<pszc;++i) {
+            printf("%d) parent %d: ", i, pai[i].il);
+            for(j=0; j<pai[i].casz; j++)
+                printf("%d ", pai[i].ca[j]);
+            printf("\n"); 
+        }
+        printf("----------------------------\n"); 
+
+        for(i=0;i<pszc;++i)
+            free(pai[i].ca);
+        free(pai);
+
+        pcpc++;
+        if(pcpc==pgbuf) {
+            pgbuf += GBUF;
+            pca=realloc(pca, pgbuf*sizeof(int));
+        }
+        pca[pcpc]=pszc;
     }
 
-    free(ra);
-
-    for(i=0;i<pszc;++i) {
-        printf("%d) parent %d: ", i, pai[i].il);
-        for(j=0; j<pai[i].casz; j++)
-            printf("%d ", pai[i].ca[j]);
-        printf("\n"); 
-    }
-
-    for(i=0;i<pszc;++i)
-        free(pai[i].ca);
-    free(pai);
-
+    free(pca);
     return 0;
 }
